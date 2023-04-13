bd9d513f9c7b6e6edb5b00ba04d6ec71ba80e4c0
rene <renegrooth@gmail.com>
2023-04-13 17:14:36 +1200

    started menu

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,14 @@
 import Canvas from './main'
+import Home from './Home'
 import P5 from './main'
 
 function App() {
   return (
     <div>
+      <div style={{transform:translate"0px-100px"}}>
+      <Home />
+
+      </div>
       <Canvas />
     </div>
   )
---
+++ client/components/Home.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function Home() {
+  return (
+    <div>
+      <ul className="menu">
+        <li>Start</li>
+        <li>Shop</li>
+        <li>LeaderBoard</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
