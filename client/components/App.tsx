1dad7163b48825852f73e42ce0d95bd1c634e696
rene <renegrooth@gmail.com>
2023-04-13 18:05:26 +1200

    Added border and menu disappers on start

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,19 @@
 import Canvas from './main'
 import Home from './Home'
 import P5 from './main'
+import { useState } from 'react'
 
 function App() {
+  const [showHome, setShowHome] = useState(true) // state variable to show/hide Home component
+
+  const handleStart = () => {
+    setShowHome(false) // hide Home component when Start button is clicked
+  }
+
   return (
     <div>
-      <div>
-        <Home />
-      </div>
+      {showHome && <Home onStart={handleStart} />}{' '}
+      {/* pass handleStart function as a prop to Home component */}
       <Canvas />
     </div>
   )
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 
-function Home() {
+interface HomeProps {
+  onStart: () => void
+}
+
+function Home(props: HomeProps) {
+  const handleStartClick = () => {
+    props.onStart()
+  }
+
   return (
     <div className="menu-overlay">
-      <ul className="menu">
-        <li>
-          <button>Start</button>
-        </li>
-        <li>
-          <button>Shop</button>
-        </li>
-        <li>
-          <button>LeaderBoard</button>
-        </li>
-      </ul>
+      <div className="border">
+        <ul className="menu">
+          <li>
+            <button onClick={handleStartClick}>Start</button>
+          </li>
+          <li>
+            <button>Shop</button>
+          </li>
+          <li>
+            <button>LeaderBoard</button>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
