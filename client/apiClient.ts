2e8a31501986944fc7dec8f3b79d295e57a11b5e
rene <renegrooth@gmail.com>
2023-04-15 14:16:21 +1200

    added post route and name component

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -4,3 +4,8 @@ export async function getLeaderboard() {
   const response = await request.get('/api/leaderboard')
   return response.body
 }
+
+export async function addToLeaderboard(newScore: any) {
+  const id = await request.post('/api/leaderboard').send(newScore)
+  return id
+}
