e29e38d33d08649ed4591d0e3d93d6e8f98cc977
edwards <james.edwards5566@gmail.com>
2023-04-17 08:41:18 +1200

    mege me

--- client/components/game/checkCollision.tsx
+++ client/components/game/checkCollision.tsx
@@ -1,71 +1,76 @@
-
-
-
 //holly fuck this actually works
-    function compare(line1,line2){
-        const vector1 = [{x: line1[0][0], y: line1[0][1]}, {x: line1[1][0], y: line1[1][1]}];
-        const vector2 = [{x: line2[0][0], y: line2[0][1]}, {x: line2[1][0], y: line2[1][1]}];
-        return doIntersect(vector1[0], vector1[1], vector2[0], vector2[1])
-    }
+//this is not being used anywhere
+function compare(line1, line2) {
+  const vector1 = [
+    { x: line1[0][0], y: line1[0][1] },
+    { x: line1[1][0], y: line1[1][1] },
+  ]
+  const vector2 = [
+    { x: line2[0][0], y: line2[0][1] },
+    { x: line2[1][0], y: line2[1][1] },
+  ]
+  return doIntersect(vector1[0], vector1[1], vector2[0], vector2[1])
+}
 
-    function onSegment(p, q, r) {
-        if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
-            q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
-          return true;
-        return false;
-      }
-      
-      function orientation(p, q, r) {
-        const val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-        if (val === 0) return 0;
-        return (val > 0) ? 1 : 2;
-      }
-      
-      function doIntersect(p1, q1, p2, q2) {
-        
-        const o1 = orientation(p1, q1, p2);
-        const o2 = orientation(p1, q1, q2);
-        const o3 = orientation(p2, q2, p1);
-        const o4 = orientation(p2, q2, q1);
-      
-        if (o1 !== o2 && o3 !== o4)
-          return true;
-      
-        if (o1 === 0 && onSegment(p1, p2, q1)) return true;
-        if (o2 === 0 && onSegment(p1, q2, q1)) return true;
-        if (o3 === 0 && onSegment(p2, p1, q2)) return true;
-        if (o4 === 0 && onSegment(p2, q1, q2)) return true;
-      
-        return false;
-      }
+function onSegment(p, q, r) {
+  if (
+    q.x <= Math.max(p.x, r.x) &&
+    q.x >= Math.min(p.x, r.x) &&
+    q.y <= Math.max(p.y, r.y) &&
+    q.y >= Math.min(p.y, r.y)
+  )
+    return true
+  return false
+}
 
-      
+function orientation(p, q, r) {
+  const val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+  if (val === 0) return 0
+  return val > 0 ? 1 : 2
+}
+
+function doIntersect(p1, q1, p2, q2) {
+  const o1 = orientation(p1, q1, p2)
+  const o2 = orientation(p1, q1, q2)
+  const o3 = orientation(p2, q2, p1)
+  const o4 = orientation(p2, q2, q1)
+
+  if (o1 !== o2 && o3 !== o4) return true
+
+  if (o1 === 0 && onSegment(p1, p2, q1)) return true
+  if (o2 === 0 && onSegment(p1, q2, q1)) return true
+  if (o3 === 0 && onSegment(p2, p1, q2)) return true
+  if (o4 === 0 && onSegment(p2, q1, q2)) return true
+
+  return false
+}
+
 function checkCollision(bounds1, bounds2) {
-   const lines1 = [
-     [bounds1.tl, bounds1.tr],
-     [bounds1.tl, bounds1.bl],
-     [bounds1.bl, bounds1.br],
-     [bounds1.br, bounds1.tr],
-   ];
- 
-   const lines2 = [
-     [bounds2.tl, bounds2.tr],
-     [bounds2.tl, bounds2.bl],
-     [bounds2.bl, bounds2.br],
-     [bounds2.br, bounds2.tr],
-   ];
- 
-   const intersections = [];
- 
-   for (let i = 0; i < lines1.length; i++) {
-     for (let j = 0; j < lines2.length; j++) {
-       if (compare(lines1[i], lines2[j])) {
-         intersections.push([lines1[i], lines2[j]]);
-       }
-     }
-   }
- 
-   return intersections;
- }
+  const lines1 = [
+    [bounds1.tl, bounds1.tr],
+    [bounds1.tl, bounds1.bl],
+    [bounds1.bl, bounds1.br],
+    [bounds1.br, bounds1.tr],
+  ]
 
- export default checkCollision
+  const lines2 = [
+    [bounds2.tl, bounds2.tr],
+    [bounds2.tl, bounds2.bl],
+    [bounds2.bl, bounds2.br],
+    [bounds2.br, bounds2.tr],
+  ]
+
+  const intersections = []
+
+  for (let i = 0; i < lines1.length; i++) {
+    for (let j = 0; j < lines2.length; j++) {
+      if (compare(lines1[i], lines2[j])) {
+        intersections.push([lines1[i], lines2[j]])
+      }
+    }
+  }
+
+  return intersections
+}
+
+export default checkCollision
--- client/components/game/platform.tsx
+++ client/components/game/platform.tsx
@@ -1,91 +1,81 @@
 class platform {
   constructor(pos) {
-    pos[0]=[pos[0][0]*50,pos[0][1]*50]
+    pos[0] = [pos[0][0] * 50, pos[0][1] * 50]
     this.topCorner = pos[0]
     this.bottomCorner = pos[1]
 
-    this.bounds={
-      tl:pos[0],
-      tr:[pos[0][0]+pos[1][0],pos[0][1]],
-      bl:[pos[0][0],pos[0][1]+pos[1][1]],
-      br:[pos[0][0]+pos[1][0],pos[0][1]+pos[1][1]]
+    this.bounds = {
+      tl: pos[0],
+      tr: [pos[0][0] + pos[1][0], pos[0][1]],
+      bl: [pos[0][0], pos[0][1] + pos[1][1]],
+      br: [pos[0][0] + pos[1][0], pos[0][1] + pos[1][1]],
     }
   }
 
-  deleteOffCamera(y){
-    if(this.topCorner[1]>y){
+  deleteOffCamera(y) {
+    if (this.topCorner[1] > y) {
       return false
     }
     return true
-
   }
-   collision(player) {
-
-    // if(
-    //   player.bounds.tl[0]+5<this.bounds.tr[0]&&
-    //   this.bounds.tr[1]+5<player.bounds.tl[1]&&
-    //   player.bounds.tl[1]+5<this.bounds.br[1]&&
-
-    //   player.bounds.tl[0]>this.bounds.br[0]-20
-
-    // ){
-    //   console.log("right collision")
-    //   player.pos[0]=player.pos[0]+1
-    //   player.velocity[0]=-player.velocity[0]/2
-    //   return player
-    // }
-    // if(
-    //   player.bounds.tr[0]>this.bounds.tl[0]&&
-    //   this.bounds.tl[1]<player.bounds.tr[1]&&
-    //   player.bounds.tr[1]<this.bounds.bl[1]&&
-
-
-    //   player.bounds.tr[0]<this.bounds.bl[0]+20
-
-    // ){
-    //   console.log("left collision")
-    //   player.pos[0]=player.pos[0]-.2
-    //   player.velocity[0]=-player.velocity[0]/4
-    //   return player
-    // }
-
-  
-     if (
-      this.bounds.tl[0]<player.bounds.bl[0]&&//this handles top collision// there is probably a better way to do this.
-      player.bounds.bl[0]<this.bounds.tr[0]&&//chunkey if stat,ems
-     this.bounds.tr[1]< player.bounds.bl[1]&&
-     this.bounds.tr[1]+this.bottomCorner[1]/2> player.bounds.bl[1]
-     ||
-     this.bounds.tl[0]<player.bounds.br[0]&&
-     player.bounds.br[0]<this.bounds.tr[0]&&
-    this.bounds.tr[1]< player.bounds.br[1]&&
-    this.bounds.tr[1]+this.bottomCorner[1]/2> player.bounds.br[1]
+  collision(player) {
+    if (
+      player.bounds.tl[0] + 5 < this.bounds.tr[0] &&
+      this.bounds.tr[1] + 5 < player.bounds.tl[1] &&
+      player.bounds.tl[1] + 5 < this.bounds.br[1] &&
+      player.bounds.tl[0] > this.bounds.br[0] - 20
     ) {
-      player.velocity[1] = -player.velocity[1]*.5
-      player.pos[1] =this.bounds.tl[1]-player.size
-      player.grounded=true
+      console.log('right collision')
+      player.pos[0] = player.pos[0] + 1
+      player.velocity[0] = -player.velocity[0] / 2
+      return player
+    }
+    if (
+      player.bounds.tr[0] > this.bounds.tl[0] &&
+      this.bounds.tl[1] < player.bounds.tr[1] &&
+      player.bounds.tr[1] < this.bounds.bl[1] &&
+      player.bounds.tr[0] < this.bounds.bl[0] + 20
+    ) {
+      console.log('left collision')
+      player.pos[0] = player.pos[0] - 0.2
+      player.velocity[0] = -player.velocity[0] / 4
       return player
     }
 
     if (
-      this.bounds.bl[0]<player.bounds.tl[0]&&//this handles top collision// there is probably a better way to do this.
-      player.bounds.tl[0]<this.bounds.br[0]&&
-     this.bounds.br[1]-this.bottomCorner[1]/2< player.bounds.tl[1]&&
-     this.bounds.br[1]>player.bounds.tl[1]
-     ||
-     this.bounds.bl[0]<player.bounds.tr[0]&&
-     player.bounds.tr[0]<this.bounds.br[0]&&
-    this.bounds.br[1]-this.bottomCorner[1]/2< player.bounds.tr[1]&&
-    this.bounds.br[1]>player.bounds.tr[1]
+      (this.bounds.tl[0] < player.bounds.bl[0] && //this handles top collision// there is probably a better way to do this.
+        player.bounds.bl[0] < this.bounds.tr[0] && //chunkey if stat,ems
+        this.bounds.tr[1] < player.bounds.bl[1] &&
+        this.bounds.tr[1] + this.bottomCorner[1] / 2 > player.bounds.bl[1]) ||
+      (this.bounds.tl[0] < player.bounds.br[0] &&
+        player.bounds.br[0] < this.bounds.tr[0] &&
+        this.bounds.tr[1] < player.bounds.br[1] &&
+        this.bounds.tr[1] + this.bottomCorner[1] / 2 > player.bounds.br[1])
     ) {
-      player.pos[1] =this.bounds.bl[1]
+      player.velocity[1] = -player.velocity[1] * 0.5
+      player.pos[1] = this.bounds.tl[1] - player.size
+      player.grounded = true
+      return player
+    }
+
+    if (
+      (this.bounds.bl[0] < player.bounds.tl[0] && //this handles top collision// there is probably a better way to do this.
+        player.bounds.tl[0] < this.bounds.br[0] &&
+        this.bounds.br[1] - this.bottomCorner[1] / 2 < player.bounds.tl[1] &&
+        this.bounds.br[1] > player.bounds.tl[1]) ||
+      (this.bounds.bl[0] < player.bounds.tr[0] &&
+        player.bounds.tr[0] < this.bounds.br[0] &&
+        this.bounds.br[1] - this.bottomCorner[1] / 2 < player.bounds.tr[1] &&
+        this.bounds.br[1] > player.bounds.tr[1])
+    ) {
+      player.pos[1] = this.bounds.bl[1]
       player.velocity[1] = -player.velocity[1]
       return player
     }
     return player
   }
   draw(p5) {
-    const c=[25, Math.random() * 50, 100]
+    const c = [25, Math.random() * 50, 100]
     p5.fill(c)
     p5.stroke(c)
     p5.rect(
